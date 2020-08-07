d2c9d742e0ed77ab4c8bad21e6c4386de0ab9859
ZQWhiting <zach.whiting@icloud.com>
2020-08-06 20:36:31 -0600

    created Employee table

---
+++ models/Employee.js
@@ -0,0 +1,66 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class Employee extends Model {
+	checkPassword(loginPw) {
+		return bcrypt.compareSync(loginPw, this.password);
+	}
+}
+
+Employee.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		firstname: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		lastname: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: [4],
+			},
+		},
+	},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: 'employee',
+		hooks: {
+			async beforeBulkCreate(employees) {
+				for (const employee of employees) {
+					const { password } = employee;
+					employee.password = await bcrypt.hash(password, 10);
+				}
+			},
+			async beforeCreate(newEmployeeData) {
+				newEmployeeData.password = await bcrypt.hash(
+					newEmployeeData.password,
+					10
+				);
+				return newEmployeeData;
+			},
+			async beforeUpdate(updatedEmployeeData) {
+				updatedEmployeeData.password = await bcrypt.hash(
+					updatedEmployeeData.password,
+					10
+				);
+				return updatedEmployeeData;
+			},
+		},
+	}
+);
+
+module.exports = Employee;
