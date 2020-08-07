a9f710b62200899876dfda66d684434e339081da
ZQWhiting <zach.whiting@icloud.com>
2020-08-06 20:58:13 -0600

    added Calendar model

--- models/index.js
+++ models/index.js
@@ -1,5 +1,15 @@
 const Employee = require('./Employee');
+const Calendar = require('./Calendar');
+
+Employee.hasOne(Calendar, {
+	foreignKey: 'employee_id',
+});
+
+Calendar.belongsTo(Employee, {
+	foreignKey: 'employee_id',
+});
 
 module.exports = {
 	Employee,
+	Calendar,
 };
