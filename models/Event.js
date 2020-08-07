1d9c6e1951b9563fb471b883bcc791879b3d3299
ZQWhiting <zach.whiting@icloud.com>
2020-08-06 21:15:20 -0600

    created event model

---
+++ models/Event.js
@@ -0,0 +1,52 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Event extends Model {}
+
+Event.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		title: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		description: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		date: {
+			type: DataTypes.DATEONLY,
+			allowNull: false,
+		},
+		start_time: {
+			type: DataTypes.TIME,
+			allowNull: false,
+		},
+		end_time: {
+			type: DataTypes.TIME,
+			allowNull: false,
+		},
+		calendar_id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+		references: {
+			model: 'calendar',
+			key: 'id',
+		},
+	},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: 'calendar',
+	}
+);
+
+module.exports = Event;
--- models/index.js
+++ models/index.js
@@ -1,15 +1,24 @@
 const Employee = require('./Employee');
 const Calendar = require('./Calendar');
+const Event = require('./Event');
 
 Employee.hasOne(Calendar, {
 	foreignKey: 'employee_id',
 });
 
+Calendar.hasMany(Event, {
+	foreignKey: 'calendar_id',
+});
 Calendar.belongsTo(Employee, {
 	foreignKey: 'employee_id',
+});
+
+Event.belongsTo(Calendar, {
+	foreignKey: 'calendar_id',
 });
 
 module.exports = {
 	Employee,
 	Calendar,
+	Event,
 };
