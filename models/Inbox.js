f7a78934aa24f0592f53f11960a24bfcd0015f3c
ZQWhiting <zach.whiting@icloud.com>
2020-08-11 11:32:14 -0600

    added Inbox model

---
+++ models/Inbox.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Inbox extends Model {}
+
+Inbox.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		employee_id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			references: {
+				model: 'employee',
+				key: 'id',
+			},
+		},
+	},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: 'inbox',
+	}
+);
+
+module.exports = Inbox;
