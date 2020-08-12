d174c08e327216f5854fc81091c1c56da3295ad9
Spartacus <k_morter@outlook.com>
2020-08-11 19:48:43 -0600

    added get routes for api/calendar

---
+++ controllers/api/calendar-routes.js
@@ -0,0 +1,66 @@
+const router = require('express').Router();
+const { Calendar, Employee, Event } = require('../../models');
+
+//get all calendars
+router.get('/', (req, res) => {
+    Calendar.findAll({
+        order: [['created_at', 'DESC']],
+        attributes: ['id', 'employee_id'],
+        include: [
+            {
+                model: Event,
+                attributes: ['id', 'title', 'description', 'date', 'start_time', 'end_time'],
+                include: {
+                    model: Employee,
+                    attributes: ['firstname', 'lastname', 'email']
+                }
+            },
+            {
+                model: Employee,
+                attributes: ['firstname', 'lastname', 'email']
+            }
+        ]
+    })
+    .then(dbCalendarData => res.json(dbCalendarData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//get a single calendar
+router.get('/:id', (req, res) => {
+    Calendar.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'employee_id', 'created_at'],
+        include: [
+            {
+                model: Event,
+                attributes: ['id', 'title', 'description', 'date', 'start_time', 'end_time'],
+                include: {
+                    model: Employee,
+                    attributes: [ 'firstname', 'lastname', 'email' ]
+                }
+            },
+            {
+                model: Employee,
+                attributes: ['firstname', 'lastname', 'email']
+            }
+        ]
+    })
+    .then(dbCalendarData => {
+        if(!dbCalendarData) {
+            res.status(404).json({ message: 'No calendar found with this id' })
+            return;
+        }
+        res.json(dbCalendarData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
