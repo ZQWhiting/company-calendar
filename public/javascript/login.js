30757f101c4d23c7bd7d61519c8d0318fd4a1e7e
jeremyrice98 <ricefamily1003@gmail.com>
2020-08-13 17:46:19 -0600

    add javascript fix

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,54 +1,54 @@
 async function loginFormHandler(event) {
-    event.preventDefault();
-  
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-  
-    if (email && password) {
-      const response = await fetch('/api/employee/login', {
-        method: 'post',
-        body: JSON.stringify({
-          email,
-          password
-        }),
-            headers: { 'Content-Type': 'application/json' }
-        });
-  
-        if (response.ok) {
-            document.location.replace('/');
-        } else {
-            alert(response.statusText);
-        }
-    }
+  event.preventDefault();
+
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (email && password) {
+    const response = await fetch('/api/employee/login', {
+      method: 'post',
+      body: JSON.stringify({
+        email,
+        password
+      }),
+          headers: { 'Content-Type': 'application/json' }
+      });
+
+      if (response.ok) {
+          document.location.replace('/');
+      } else {
+          alert(response.statusText);
+      }
+  }
 }
 
 async function signupFormHandler(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const firstname = document.querySelector('#firstname-signup').value.trim();
-    const lastname = document.querySelector('#lastname-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (firstname && lastname && email && password) {
-        const response = await fetch('/api/employee', {
-        method: 'post',
-        body: JSON.stringify({
-          firstname,
-          lastname,
-          email,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      })
-        // check the response status
-        if (response.ok) {
-            console.log('success');
-        } else {
-            alert(response.statusText);
-        }
-    }
+  const firstname = document.querySelector('#firstname-signup').value.trim();
+  const lastname = document.querySelector('#lastname-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (firstname && lastname && email && password) {
+      const response = await fetch('/api/employee', {
+      method: 'post',
+      body: JSON.stringify({
+        firstname,
+        lastname,
+        email,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    })
+      // check the response status
+      if (response.ok) {
+          console.log('success');
+      } else {
+          alert(response.statusText);
+      }
   }
-  
-  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
-  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+}
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
