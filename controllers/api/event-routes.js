ee359b64513397a684e32d4bd57c42f6c2da05d9
ZQWhiting <zach.whiting@icloud.com>
2020-08-20 17:24:04 -0600

    added authentication to adding and deleting events

--- controllers/api/event-routes.js
+++ controllers/api/event-routes.js
@@ -1,49 +1,70 @@
 const router = require('express').Router();
 const { Event, Employee } = require('../../models');
+const session = require('express-session');
 
 // get event
 router.get('/', (req, res) => {
-    Event.findAll({
-        attributes: ['id', 'title', 'description', 'date', 'start_time', 'end_time', 'calendar_id', 'employee_id'],
-        include: [
-            {
-                model: Employee,
-                attributes: ['firstname', 'lastname', 'email']
-            }
-        ]
-    })
-    .then(dbEventData => res.json(dbEventData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+	Event.findAll({
+		attributes: [
+			'id',
+			'title',
+			'description',
+			'date',
+			'start_time',
+			'end_time',
+			'calendar_id',
+			'employee_id',
+		],
+		include: [
+			{
+				model: Employee,
+				attributes: ['firstname', 'lastname', 'email'],
+			},
+		],
+	})
+		.then((dbEventData) => res.json(dbEventData))
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
 });
 
 //get a single event
 router.get('/:id', (req, res) => {
-    Event.findOne({
-        where: {
-            id: req.params.id
-        },
-        attributes: ['id', 'title', 'description', 'date', 'start_time', 'end_time', 'calendar_id', 'employee_id'],
-        include: [
-            {
-                model: Employee,
-                attributes: ['firstname', 'lastname', 'email']
-            }
-        ]
-    })
-    .then(dbEventData => {
-        if(!dbEventData) {
-            res.status(404).json({ message: 'No event found with this id' })
-            return;
-        }
-        res.json(dbEventData);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+	Event.findOne({
+		where: {
+			id: req.params.id,
+		},
+		attributes: [
+			'id',
+			'title',
+			'description',
+			'date',
+			'start_time',
+			'end_time',
+			'calendar_id',
+			'employee_id',
+		],
+		include: [
+			{
+				model: Employee,
+				attributes: ['firstname', 'lastname', 'email'],
+			},
+		],
+	})
+		.then((dbEventData) => {
+			if (!dbEventData) {
+				res.status(404).json({
+					message: 'No event found with this id',
+				});
+				return;
+			}
+			res.json(dbEventData);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
 });
 
 //create an event
@@ -58,7 +79,7 @@ router.post('/', (req, res) => {
 			end_time: req.body.end_time,
 			calendar_id: req.body.calendar_id,
 			// use the id from the session
-			employee_id: req.body.employee_id,
+			employee_id: req.session.employee_id,
 		})
 			.then((dbEventData) => res.json(dbEventData))
 			.catch((err) => {
@@ -103,51 +124,55 @@ router.post('/:id/calendar/:calendar_id', (req, res) => {
 
 //update an event
 router.put('/:id', (req, res) => {
-    Event.update(
-        {
-            title: req.body.title,
-            description: req.body.description,
-            date: req.body.date,
-            start_time: req.body.start_time,
-            end_time: req.body.end_time
-        },
-        {
-            where: {
-                id: req.params.id
-            }
-        }
-    )
-    .then(dbEventData => {
-        if (!dbEventData) {
-          res.status(404).json({ message: 'No event found with this id' });
-          return;
-        }
-        res.json(dbEventData);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+	Event.update(
+		{
+			title: req.body.title,
+			description: req.body.description,
+			date: req.body.date,
+			start_time: req.body.start_time,
+			end_time: req.body.end_time,
+		},
+		{
+			where: {
+				id: req.params.id,
+			},
+		}
+	)
+		.then((dbEventData) => {
+			if (!dbEventData) {
+				res.status(404).json({
+					message: 'No event found with this id',
+				});
+				return;
+			}
+			res.json(dbEventData);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
 });
 
 //delete an event
 router.delete('/:id', (req, res) => {
-    Event.destroy({
-        where: {
-            id: req.params.id
-        }
-    })
-    .then(dbEventData => {
-        if (!dbEventData) {
-            res.status(404).json({ message: 'No event found with this id' });
-            return;
-        }
-        res.json(dbEventData);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+	Event.destroy({
+		where: {
+			id: req.params.id,
+		},
+	})
+		.then((dbEventData) => {
+			if (!dbEventData) {
+				res.status(404).json({
+					message: 'No event found with this id',
+				});
+				return;
+			}
+			res.json(dbEventData);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
 });
 
 module.exports = router;
