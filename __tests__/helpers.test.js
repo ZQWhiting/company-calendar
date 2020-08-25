e84bbb3d585b1553d2ac94b415a7a6f8c9677d23
ZQWhiting <zach.whiting@icloud.com>
2020-08-24 19:45:06 -0600

    added format_time helper

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,7 +1,15 @@
-const {format_date} = require('../utils/helpers');
+const { format_date, format_time } = require('../utils/helpers');
 
-test('format_date() returns a date string', () => {
-    const date = new Date('2020-08-20 16:12:03');
-  
-    expect(format_date(date)).toBe('Sunday, 8/20/2020');
+// test('format_date() returns a date string', () => {
+// 	const date = new Date('2020-08-20 16:12:03');
+
+// 	expect(format_date(date)).toBe('Sunday, 8/20/2020');
+// });
+
+test('format_time() returns a time string', () => {
+	const time = '15:10:00';
+	const time2 = '7:15:00';
+
+	expect(format_time(time)).toBe('3:10 PM');
+	expect(format_time(time2)).toBe('7:15 AM');
 });
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,26 +1,42 @@
 module.exports = {
 	format_date_long: (date) => {
 		var month = new Array();
-		month[0] = "January";
-		month[1] = "February";
-		month[2] = "March";
-		month[3] = "April";
-		month[4] = "May";
-		month[5] = "June";
-		month[6] = "July";
-		month[7] = "August";
-		month[8] = "September";
-		month[9] = "October";
-		month[10] = "November";
-		month[11] = "December";
-		
+		month[0] = 'January';
+		month[1] = 'February';
+		month[2] = 'March';
+		month[3] = 'April';
+		month[4] = 'May';
+		month[5] = 'June';
+		month[6] = 'July';
+		month[7] = 'August';
+		month[8] = 'September';
+		month[9] = 'October';
+		month[10] = 'November';
+		month[11] = 'December';
+
 		return `
-		${month[new Date(date).getMonth()]}, ${new Date(date).getDate() + 1} ${new Date(date).getFullYear()}`;
+		${month[new Date(date).getMonth()]}, ${new Date(date).getDate() + 1} ${new Date(
+			date
+		).getFullYear()}`;
 	},
 
 	format_date_short: (date) => {
 		return `
-		${new Date(date).getMonth() + 1}/${new Date(date).getDate() + 1}/${new Date(date).getFullYear()}`;
+		${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
+			date
+		).getFullYear()}`;
+	},
+
+	format_time: (time) => {
+		AMorPM = 'AM';
+
+		time = time.split(':');
+		if (time[0] >= 12) {
+			time[0] -= 12;
+			AMorPM = 'PM';
+		}
+
+		return `${time[0]}:${time[1]} ${AMorPM}`;
 	},
 
 	user_authentication: (user_id, employee_id) => {
@@ -28,5 +44,5 @@ module.exports = {
 			return false;
 		}
 		return true;
-	}
+	},
 };
